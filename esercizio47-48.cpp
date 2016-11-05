e089583d87c73396a2c00f44dba50faa8484e04d
betao3 <03.betti@gmail.com>
2016-11-05 16:17:32 +0100

    Add files via upload

---
+++ esercizio47-48.cpp
@@ -0,0 +1,21 @@
+/* Federico Bettiolo
+   4^C  5/11/16    */
+
+
+
+   #include <iostream>
+
+   using namespace std;
+
+   enum {gennaio=1,febbraio,marzo,aprile,maggio,giugno,
+         luglio,agosto,settembre,ottobre,novembre,dicembre};
+
+
+   int main () {
+
+
+       cout <<" i numeri corrispondenti ai mesi di vacanza sono " <<endl;
+       cout <<giugno <<"-" <<luglio <<"-" <<agosto <<endl;
+
+       return 0;}
+
